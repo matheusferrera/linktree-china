94240a86d3e8da42e69de7e0b73fe8fb0bc5c35a
Matheus Ferreira <matheusalmeida@ibict.br>
2024-09-07 20:41:02 -0300

    log

--- src/service.js
+++ src/service.js
@@ -1,31 +1,27 @@
 // services/apiService.js
 
+import axios from 'axios';
+
 // const API_URL = 'http://localhost:3000/wpp';
 const API_URL = 'https://13.49.74.201:3000/wpp';
 
 export const sendMessage = async (phoneNumber, message) => {
-    alert(JSON.stringify({
+    const data = {
         remoteId: `55${phoneNumber.replace(/\D/g, '')}@c.us`,
         message,
-    }))
+    };
+
+    alert(JSON.stringify(data));
+
     try {
-        const response = await fetch(API_URL, {
-            method: 'POST',
+        const response = await axios.post(API_URL, data, {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                remoteId: `55${phoneNumber.replace(/\D/g, '')}@c.us`,
-                message,
-            }),
         });
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        return await response.json();
+        return response.data;
     } catch (error) {
         console.error('Error:', error);
         throw error;
